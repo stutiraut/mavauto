2819a8495cba4d68abb9d37a3969e9cf3148f370
stutiraut <sraut@unomaha.edu>
2020-02-14 00:19:50 -0600

    Added password change

--- crm/urls.py
+++ crm/urls.py
@@ -25,6 +25,11 @@ urlpatterns = [
     path('map/store_location/',views.store_location, name='store_location'),
     url(r'^register/$', views.register, name='register'),
 
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 
 
 ]
